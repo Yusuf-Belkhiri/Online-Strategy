f2d5615a93de025cfdd5dca85b64eca8f2200253
Yusuf-Belkhiri <youcefbelkhiri005@gmail.com>
2023-05-08 10:11:21 +0100

    SpawnUnits System
    Spawn UnitSpawner in: OnServerAddPlayer (owned by client, through: conn)
    UnitSpawner can spawn Unit objects (owned by the same owner client, through: connectionToClient)

---
+++ OnlineStrategy/Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Mirror;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler       // for IPointerClickHandler (originally used with UI): add EventSystem object in the scene, & Attach PhysicsRaycaster component to camera
+{
+    [SerializeField] private GameObject _unitPrefab;
+    [SerializeField] private Transform _unitSpawnPos;
+    #region SERVER
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        // First, Instantiate
+        GameObject unitInstance = GameObject.Instantiate(_unitPrefab, _unitSpawnPos.position, _unitSpawnPos.rotation);
+        // Then, Spawn it in the Server
+        NetworkServer.Spawn(unitInstance, connectionToClient);      // Specify the client this unit belongs to, or, if no network connection is passed: no ownership (server only object). 
+                                                                    // connectionToClient: (pass ownership) to make the owner of the unit the same owner of this UnitSpawner (isOwned)
+                                
+    }
+    #endregion
+
+    #region CLIENT
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left)  return;
+        
+        // It won't work anyways if another non owner client of this object click on it, Because CLIENT CAN ONLY CALL COMMANDS if HE OWNS THE OBJECT (safe) 
+        // This is just to not even bother telling the server to try to do it if we don't own this unit spawner
+        if(!isOwned) return;
+        
+        CmdSpawnUnit();
+    }
+    
+    #endregion
+
+}
--- OnlineStrategy/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ OnlineStrategy/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,8 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
+    [SerializeField] private GameObject _unitSpawnerPrefab;
+    
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);       // Instantiate & Spawn player object
+        
+        // Instantiate, & Spawn a unit spawner for the client (owned)
+        GameObject unitSpawnerInstance = 
+            GameObject.Instantiate(_unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawnerInstance, conn);     // Pass the Ownership authority (conn) (this UnitSpawner is owned by this client) 
+    }
 }
